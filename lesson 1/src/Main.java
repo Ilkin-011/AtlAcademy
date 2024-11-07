dd0e9d4d8463ca91490fddff190589bab069ed75
Ilkin011 <mirzyevilkin7@gmail.com>
2024-11-07 16:26:37 +0400

    tapsiriq

---
+++ lesson 1/src/Main.java
@@ -0,0 +1,18 @@
+import java.awt.desktop.SystemEventListener;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner scanner=new System.in;
+        System.out.println("Ilk eded")
+                int a= scanner.nextInt();
+        System.out.println("a");
+        System.out.println("Ikinci eded");
+        int b=scanner.nextInt();
+        System.out.println("b");
+        int cixma a-b;
+
+
+
+    }
+}
